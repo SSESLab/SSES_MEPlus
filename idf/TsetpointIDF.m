b1f88d39a07ef5383ca2e079524fe672dc5d5e74
Zahra-Fallahi <zahra.fallahiarezoudar@gmail.com>
2017-12-13 12:04:51 -0700

    tset point new

--- idf/TsetpointIDF.m
+++ idf/TsetpointIDF.m
@@ -2,9 +2,9 @@ function data=TsetpointIDF(data,T_band)
 j=0;
 aa=cell(2,2);
 for i=1:length(data)
-    if strfind(data(i).class,'Schedule:Compact')
-        if (cellfun(@any,(strfind((data(i).fields(1)),'CLGSETP_SCH')))...
-                ||cellfun(@any,(strfind((data(i).fields(1)),'HTGSETP_SCH'))))      
+    if strfind(data(i).class,'Schedule:Day:Interval')
+        if (cellfun(@any,(strfind((data(i).fields(1)),'CLGSETP_SCH_NO_OPTIMUM Wkdy Day')))...
+                ||cellfun(@any,(strfind((data(i).fields(1)),'HTGSETP_SCH_NO_OPTIMUM Wkdy Day'))))      
             j=j+1;
             aa{j,1}=i;
             aa{j,2}=cell2mat((data(i).fields(1)));
@@ -12,12 +12,12 @@ for i=1:length(data)
     end
 end
 if cell2mat(strfind(aa(2,2),'HTGSETP_SCH'))
-    data(aa{1,1}).fields([8,15])={num2str(T_band(2))};
-    data(aa{2,1}).fields([8,18,23])={num2str(T_band(1))};
+    data(aa{1,1}).fields(7)={num2str(T_band(1))};
+    data(aa{2,1}).fields(7)={num2str(T_band(2))};
 else
     
-    data(aa{1,1}).fields([8,15])={num2str(T_band(2))};
-    data(aa{2,1}).fields([8,18,23])={num2str(T_band(1))};
+    data(aa{1,1}).fields(7)={num2str(T_band(1))};
+    data(aa{2,1}).fields(7)={num2str(T_band(2))};
 end
 
 
